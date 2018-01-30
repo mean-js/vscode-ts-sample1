00d86956661f0a91b153eaa773d39c63b6f99677
santosh <santosh.ece06@gmail.com>
2018-01-30 18:58:20 +0530

    express with typescript.

--- src/app.ts
+++ src/app.ts
@@ -4,12 +4,10 @@ const port =  3010;
 
 App.set('port', port);
 
-//create a server and pass our Express app to it.
-const server = http.createServer(App);
-server.listen(port);
-server.on('listening', onListening);
-
-//function to note that Express is listening
-function onListening(): void {
-    console.log(`Listening on port `+port);
-}
+App.listen(port, (err) => {
+    if (err) {
+      return console.log(err)
+    }
+  
+    return console.log(`server is listening on ${port}`)
+})
---
+++ src/readme.txt
@@ -0,0 +1,5 @@
+>> REFERENCES
+    >> https://github.com/BrianDGLS/express-ts
+
+    >> https://blog.pusher.com/use-typescript-with-node/
+    >> https://blog.risingstack.com/building-a-node-js-app-with-typescript-tutorial/
